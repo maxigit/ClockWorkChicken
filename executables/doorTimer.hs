cac5cbc35a951704dc356cb881e36503de4f9020
max <bmx007@gmail.com>
2015-11-06 21:35:40 +0000

    Add initial location

--- executables/doorTimer.hs
+++ executables/doorTimer.hs
@@ -10,12 +10,15 @@ main = do
   utc <- getCurrentTime
   zone <- getCurrentTimeZone
 
-  let state = GlobalState utc zone Open Open)
+  let state = GlobalState utc zone
+                  (20/60) (53.75)
+                  0 0 -- offsets
+                  Open Open
 
   loop 1000 mainLoop state where
   mainLoop state = do
     utc <- getCurrentTime
-    let newState = setTime utc state
+    let newState = state {currentTime = utc }
     print newState
     return newState
 
