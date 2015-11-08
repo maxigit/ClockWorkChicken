a04a6e920d189b430905150d13726bd095e793b9
max <bmx007@gmail.com>
2015-11-08 20:32:39 +0000

    Split GlobalState into  World, Config and Pi

--- src/CWC.hs
+++ src/CWC.hs
@@ -8,27 +8,31 @@ import qualified Data.Time.Horizon as H
 data DoorState = Open | Closed
                 deriving (Show, Read)
 
-data WorldState = WorldState
+data WorldState = WorldState 
+  { currentTime :: UTCTime
+  }
 data Event = Even
 data PiState = PiState
 
 
-data GlobalState = GlobalState
-  { currentTime :: UTCTime
-  , timeZone :: TimeZone
+data Config = Config
+  { timeZone :: TimeZone
   , longitute :: H.LongitudeWest
   , latitude :: H.LatitudeNorth
   , sunsetOffset :: Integer
   , sunriseOffset :: Integer
-  , doorState :: DoorState
-  , doorDesiredState :: DoorState
   } deriving (Show, Read)
 
+data GlobalState = GlobalState 
+  { config :: Config
+  , world :: WorldState
+  , pi :: PiState
+  }
 
 utcZone = hoursToTimeZone 0
 minutesToDiffTime = fromInteger . (60*)
 
-currentDay :: GlobalState ->  Day
+currentDay :: WorldState ->  Day
 currentDay = fmap localDay (utcToLocalTime utcZone .currentTime)
 
 utcTimeOfDay :: UTCTime -> TimeOfDay
@@ -37,17 +41,17 @@ utcTimeOfDay = localTimeOfDay . utcToLocalTime utcZone
   
 sunset, sunrise :: GlobalState -> TimeOfDay
 sunset = do
-  day <- currentDay
-  long <- longitute
-  lat <- latitude
-  offset <- minutesToDiffTime `fmap` sunsetOffset
+  day <- currentDay . world
+  long <- longitute .config
+  lat <- latitude .config
+  offset <- minutesToDiffTime `fmap` ( sunsetOffset . config)
   return $ utcTimeOfDay (addUTCTime offset (H.sunset day long lat))
 
 sunrise = do
-  day <- currentDay
-  long <- longitute
-  lat <- latitude
-  offset <- minutesToDiffTime . sunriseOffset
+  day <- currentDay . world
+  long <- longitute . config
+  lat <- latitude .config 
+  offset <- minutesToDiffTime . (sunriseOffset . config)
   return $ utcTimeOfDay ( addUTCTime offset (H.sunset day long lat))
 
 
@@ -56,14 +60,12 @@ expectedDoorState :: GlobalState -> DoorState
 expectedDoorState = do
   set <- sunset 
   rise <- sunrise 
-  currentTime <- utcTimeOfDay `fmap` currentTime
+  currentTime <- utcTimeOfDay `fmap` (currentTime . world)
   return $ if rise <= currentTime && currentTime <= set
              then Open
              else Closed
 
 -- * In
-world :: IO WorldState
-world = return WorldState
 
 -- * Out
 out :: PiState -> IO ()
@@ -76,6 +78,7 @@ step old new = Nothing
 transition :: PiState -> Event -> IO PiState
 transition pi _ = return pi
 
+-- * Automaton
 
 -- * Misc Functions
 -- | Loop the given action every n millisecond.
