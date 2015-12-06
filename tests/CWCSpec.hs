9ecf26a4fbf2bfec4ca031925b94bfd8ed3fccfa
max <bmx007@gmail.com>
2015-12-06 14:39:21 +0000

    Add sunrise/set specs. FAILS!

--- tests/CWCSpec.hs
+++ tests/CWCSpec.hs
@@ -1,22 +1,56 @@
 module CWCSpec where
 
 import CWC
+import Control.Monad.State
+import Data.Time
+import Data.Time.LocalTime
 import Test.Hspec
 import Test.HUnit
+liftIO2  action a = liftIO . action a
 
 spec :: Spec
 spec = do
   describe "Sunset/Sunrise" $ do
-    context "Paris 03 Dec 2015" $ do
-      it "should have correct sunrise" $ do
-        pending
-      it "should have correct sunset" $ do
-        pending
-    context "Paris 03 Jun 2016" $ do
-      it "should have correct sunrise" $ do
-        pending
-      it "should have correct sunset" $ do
-        pending
+    context "without time offset" $ do
+      let zone = utc
+          defConfig = Config zone
+                      (48.8567) (2.3508)
+                      0 0
+          defWorld = WorldState $ UTCTime (fromGregorian 2015 12 03) 0
+          io = error "io not defined @toto"
+          defGlobal :: GlobalState IO
+          defGlobal = GlobalState defConfig defWorld io
+      
+
+      context "Paris 03 Dec 2015" $ do
+        let parisSunrise = TimeOfDay 08 25 00
+            parisSunset = TimeOfDay 16 56 00
+            global = defGlobal { world = defWorld { currentTime =
+              UTCTime (fromGregorian 2015 12 03) 0 }}
+            shouldBe' = liftIO2 shouldBe
+
+        it "should have correct sunrise" $ flip evalStateT global $ do
+          rise <- sunrise
+          rise `shouldBe'` parisSunrise
+
+        it "should have correct sunset" $ flip evalStateT global $ do
+          set <- sunset
+          set `shouldBe'` parisSunset
+
+      context "Paris 03 Jun 2016" $ do
+        let parisSunrise = TimeOfDay 05 51 00
+            parisSunset = TimeOfDay 21 47 00
+            global = defGlobal { world = defWorld { currentTime =
+              UTCTime (fromGregorian 2015 06 03) 0 }}
+            shouldBe' = liftIO2 shouldBe
+
+        it "should have correct sunrise" $ flip evalStateT global $ do
+          rise <- sunrise
+          rise `shouldBe'` parisSunrise
+
+        it "should have correct sunset" $ flip evalStateT global $ do
+          set <- sunset
+          set `shouldBe'` parisSunset
 
   describe "event generation" $ do
     context "when sunrise" $ do
@@ -39,6 +73,10 @@ spec = do
 
     context "door opened" $ do
       it "should send a DoorOpened event" $ do
+        pending
+
+    context "in Sunset display mode" $ do
+      it "should come back in normal mode after 5s" $ do
         pending
 
   describe "event processing:" $ do
